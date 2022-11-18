290bc5833e6ed56973d23ab5b90c176073539b4e
Nazarii Soviak <natesoviak@gmail.com>
2022-11-18 11:41:51 +0200

    Finished the HW

--- app/src/main/java/jdbc/descriptionclasses/MaxProjectsClient.java
+++ app/src/main/java/jdbc/descriptionclasses/MaxProjectsClient.java
@@ -1,10 +1,10 @@
 package jdbc.descriptionclasses;
 
-public class MaxProjectsCount {
+public class MaxProjectsClient {
     private String name;
     private int project_count;
 
-    public MaxProjectsCount(String name, int project_count) {
+    public MaxProjectsClient(String name, int project_count) {
         this.name = name;
         this.project_count = project_count;
     }
